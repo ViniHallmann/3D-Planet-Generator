1c2bce9606c3c0574cfdc812c2bc3fa4570235f9
vinicius.hallmann@avenudecode.com <vinicius.hallmann@external.stellantis.com>
2025-12-02 19:24:16 -0300

    added default values to be used

---
+++ default-values.js
@@ -0,0 +1,11 @@
+const MOUSE_ROTATION_SPEED = 0.01;
+// const MAX_ZOOM_IN = 1;
+// const MAX_ZOOM_OUT = 15;
+const MAX_LAYERS = 10;
+const MAX_CLOUD_LAYERS = 10;
+const MAX_CLOUD_OFFSET = 0.0015;
+const MIN_PHI = 0.1;
+const MAX_PHI = Math.PI - 0.1;
+const AUTO_ROTATE = true;
+
+const _LEFT_BUTTOM_VALUE = 0
