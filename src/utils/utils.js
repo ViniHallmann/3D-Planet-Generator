9db20d5a101ed16d1fd76f5ad24fe841b4b2820d
ViniHallmann <Viniciushallmann2015@gmail.com>
2026-02-16 16:28:13 -0300

    update ease func

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -16,6 +16,10 @@ export const easing = {
         const c4 = (2 * Math.PI) / 3;
         return t === 0 ? 0 : t === 1 ? 1 : Math.pow(2, -10 * t) * Math.sin((t * 10 - 0.75) * c4) + 1;
     },
+
+    easeInCubic: (t) => {
+        return t * t * t;
+    },
     
     easeOutCubic: (t) => {
         return 1 - Math.pow(1 - t, 3);
